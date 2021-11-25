21dc4825c75d480325ffbf40ab9ed50d71cae2f8
Niels Heirbaut <niels@opilo.nl>
2021-11-25 16:42:21 +0100

    Add initial controller test

---
+++ AspNetCoreAndMassTransitIntegrationTestExample.Tests/ControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using AspNetCoreAndMassTransitIntegrationTestExample.Api;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AspNetCoreAndMassTransitIntegrationTestExample.Tests
+{
+    public class ControllerTests
+        : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public ControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Get()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/WeatherForecast");
+
+            Assert.True(response.IsSuccessStatusCode);
+        }
+    }
+}
